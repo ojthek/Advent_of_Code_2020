c5427df32f9e2a9c4490740358d7247b8ceb86f9
ojthek <oliver.j.kurz@gmail.com>
2020-12-23 14:21:50 +0100

    Solved puzzle 20 part 1

--- Puzzle_20/solve.py
+++ Puzzle_20/solve.py
@@ -1,18 +1,17 @@
-import fileinput, pprint
+import fileinput, pprint, functools
 
 class Tile():
     def __init__(self,  id, lines):
         self.id = id
         self.tile = lines
-        self.top_edge = lines[0]
-        self.bottom_edge = lines[-1]
-        left, right = "", ""
-        for l in lines:
-            left += l[0]
-            right += l[-1]
-
-        self.left_edge = left
-        self.right_edge = right
+        self.edges = set([
+            lines[0],
+            ''.join(list([x[0] for x in lines])),
+            lines[-1],
+            ''.join(list([x[-1] for x in lines]))
+        ])
+        self.medges = set(list([edge[::-1] for edge in self.edges]))
+        self.connections = 0
 
     def rotateRight(self):
         top = self.top_edge
@@ -22,38 +21,43 @@ class Tile():
 
         self.right_edge = top
 
-        self.bottom_edge = right
-        self.bottom_edge.reverse()
+        self.bottom_edge = right[::-1]
 
         self.left_edge = bottom
 
-        self.self_top = left
-        self.self_top.reverse()
+        self.self_top = left[::-1]
+
+        return self
 
     def flipV(self):
         left = self.left_edge
         self.left_edge = self.right_edge
         self.right_edge = left
+        self.top_edge = self.top_edge[::-1]
+        self.bottom_edge = self.bottom_edge[::-1]
+
+        return self
 
     def flipH(self):
         top = self.top_edge
         self.top_edge = self.bottom_edge
         self.bottom_edge = top
+        self.left_edge = self.left_edge[::-1]
+        self.right_edge = self.right_edge[::-1]
+
+        return self
 
     def __str__(self):
-        return "\nTile(" + self.id + ")\n\ttop:\t" + self.top_edge +"\n\tright:\t" + self.right_edge + "\n\tbottom:\t" + self.bottom_edge + "\n\tleft:\t" + self.left_edge
+        return "\nTile(" + self.id + ")" #\n\ttop:\t" + self.top_edge +"\n\tright:\t" + self.right_edge + "\n\tbottom:\t" + self.bottom_edge + "\n\tleft:\t" + self.left_edge
 
     def connects(self, tile):
-        if self.top_edge == tile.bottom_edge:
-            return "top"
-        if self.right_edge == tile.left_edge:
-            return "right"
-        if self.bottom_edge == tile.top_edge:
-            return "bottom"
-        if self.left_edge == tile.right_edge:
-            return "left"
+        c = 0
+        c += len(self.edges & tile.edges)
+        c += len(self.edges & tile.medges)
+        c += len(self.medges & tile.edges)
+        c += len(self.medges & tile.medges)
 
-        return None
+        return c
 
 def collectTiles():
     tileKey = -1
@@ -69,24 +73,31 @@ def collectTiles():
             tiles.append(Tile(tileKey,tile))
             tile = []
 
-def solvePuzzle(tile, t2connect):
-    for n in t2connect:
-        c = tile.connects(n)
-        if c:
-            print("Found neighbour", c, tile, n)
-        else:
-            print("Nope")
+def solvePuzzle():
+    for tile in tiles:
+        print(tile)
+        c = 0
+        for tile2 in tiles:
+            if tile.id == tile2.id:
+                continue
+
+            count = tile.connects(tile2)
+            if count > 0:
+                print("Connections found", tile2.id, count)
+                c += 1
+        tile.connections = c
 
 tiles = []
+gC = []
 
 collectTiles()
 
-for t in tiles:
-    print(t)
 
-solvePuzzle(tiles[0], tiles[1:])
+solvePuzzle()
 
+print([(x.id, x.connections) for x in tiles])
 
+print("Result:", functools.reduce(lambda x,y: x*y, [int(x.id) for x in tiles if x.connections == 2]))
 
 
 
